715a7838844e3483925e1e3ae6da952016a39195
Sonam Muneer <sonammoony@gmail.com>
2025-04-15 20:28:36 +0530

    fix: cors enabled #4

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     allowedHeaders: '*',
-    origin: '*',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   const config = new DocumentBuilder()
