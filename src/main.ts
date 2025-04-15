30d9aa483f9fe13e48e49ddc207848136740ecba
Sonam Muneer <sonammoony@gmail.com>
2025-04-15 20:37:46 +0530

    fix: cors enabled #6

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { bodyParser: true });
   app.enableCors({
-    allowedHeaders: '*',
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   const config = new DocumentBuilder()
     .setTitle('Hotel Management System')
