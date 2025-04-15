b9924f7ec85cb7df6b09dc9ebc3fe325fbf642a0
Sonam Muneer <sonammoony@gmail.com>
2025-04-15 20:43:35 +0530

    fix: cors enabled #7

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,12 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   const config = new DocumentBuilder()
     .setTitle('Hotel Management System')
     .setDescription('API description')
